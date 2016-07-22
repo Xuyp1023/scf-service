fe5e8372310b60c0c412d15e8b1764c5c36b5a2b
root <git4bytter@sina.cn>
2016-07-22 15:42:18 +0800

    task-007  [SCF-Credit]保理商调整授信额度

--- src/main/java/com/betterjr/modules/credit/service/ScfCreditDetailService.java
+++ src/main/java/com/betterjr/modules/credit/service/ScfCreditDetailService.java
@@ -4,26 +4,9 @@ import org.springframework.stereotype.Service;
 
 import com.betterjr.common.service.BaseService;
 import com.betterjr.modules.credit.dao.ScfCreditDetailMapper;
-import com.betterjr.modules.credit.entity.ScfCredit;
 import com.betterjr.modules.credit.entity.ScfCreditDetail;
 
 @Service
 public class ScfCreditDetailService extends BaseService<ScfCreditDetailMapper, ScfCreditDetail> {
-
-    /**
-     * 授信录入变动记录
-     * 
-     * @param anCredit
-     */
-    public int addCreditDetail(ScfCredit anCredit) {
-        logger.info("Begin to add Credit Detail");
-
-        ScfCreditDetail anCreditDetail = new ScfCreditDetail();
-        anCreditDetail.initAddValue(anCredit);
-        anCreditDetail.setDirection("0");// 方向：收
-
-        // 数据存盘
-        return this.insert(anCreditDetail);
-    }
 
 }
--- src/main/java/com/betterjr/modules/credit/service/ScfCreditService.java
+++ src/main/java/com/betterjr/modules/credit/service/ScfCreditService.java
@@ -1,5 +1,6 @@
 package com.betterjr.modules.credit.service;
 
+import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -8,12 +9,17 @@ import org.springframework.stereotype.Service;
 
 import com.betterjr.common.exception.BytterTradeException;
 import com.betterjr.common.service.BaseService;
+import com.betterjr.common.utils.BTAssert;
+import com.betterjr.common.utils.BetterStringUtils;
 import com.betterjr.common.utils.Collections3;
+import com.betterjr.common.utils.MathExtend;
+import com.betterjr.common.utils.UserUtils;
 import com.betterjr.mapper.pagehelper.Page;
 import com.betterjr.modules.account.service.CustAccountService;
 import com.betterjr.modules.account.service.CustAndOperatorRelaService;
 import com.betterjr.modules.credit.dao.ScfCreditMapper;
 import com.betterjr.modules.credit.entity.ScfCredit;
+import com.betterjr.modules.credit.entity.ScfCreditDetail;
 
 @Service
 public class ScfCreditService extends BaseService<ScfCreditMapper, ScfCredit> {
@@ -64,18 +70,17 @@ public class ScfCreditService extends BaseService<ScfCreditMapper, ScfCredit> {
 
         // 初始化参数
         anCredit.initAddValue();
-
         // 设置操作员所属保理公司客户号
         anCredit.setFactorNo(Collections3.getFirst(custAndOperatorRelaService.findCustNoList(anCredit.getRegOperId(), anCredit.getOperOrg())));
-
         // 检查是否已授信
         checkCreditExists(anCredit);
-
         // 数据存盘-授信额度
         this.insert(anCredit);
-
         // 数据存盘-授信额度变动
-        scfCreditDetailService.addCreditDetail(anCredit);
+        ScfCreditDetail anCreditDetail = new ScfCreditDetail();
+        anCreditDetail.initAddValue(anCredit.getCustNo(), anCredit.getCreditLimit(), anCredit.getId());
+        anCreditDetail.setDirection("0");// 方向：0-收;1-支;
+        scfCreditDetailService.insert(anCreditDetail);
 
         return anCredit;
     }
@@ -91,6 +96,66 @@ public class ScfCreditService extends BaseService<ScfCreditMapper, ScfCredit> {
         if (Collections3.isEmpty(this.selectByProperty(anMap)) == false) {
             logger.info("当前客户已存在该授信类型的记录,不允许重复授信");
             throw new BytterTradeException(40001, "当前客户已存在该授信类型的记录,不允许重复授信");
+        }
+    }
+
+    /**
+     * 授信记录修改
+     * 
+     * @param anModiCredit
+     * @param anId
+     * @return
+     */
+    public ScfCredit saveModifyCredit(ScfCredit anModiCredit, Long anId) {
+        logger.info("Begin to modify Credit");
+
+        // 获取授信记录
+        ScfCredit anCredit = this.selectByPrimaryKey(anId);
+        BTAssert.notNull(anCredit, "无法获取授信记录");
+        // 检查当前操作员是否能修改该授信记录
+        checkOperator(anCredit.getOperOrg(), "当前操作员不能修改该授信记录");
+        // 检查授信状态,不允许修改已生效的授信记录
+        checkStatus(anCredit.getBusinStatus(), "1", true, "授信记录已生效,不允许修改");
+        // 检查授信状态,不允许修改已过期的授信记录
+        checkStatus(anCredit.getBusinStatus(), "2", true, "授信记录已过期,不允许修改");
+        // 设置修改信息
+        anModiCredit.initModifyValue(anCredit);
+        // 数据存盘-授信额度修改
+        this.updateByPrimaryKeySelective(anModiCredit);
+        // 检查授信额度是否调整
+        BigDecimal balance = MathExtend.subtract(anCredit.getCreditLimit(), anModiCredit.getCreditLimit());
+        // 授信额度发生改变
+        if (balance.longValue() != 0) {
+            ScfCreditDetail anCreditDetail = new ScfCreditDetail();
+            anCreditDetail.initModifyValue(anCredit.getCustNo(), anCredit.getId());
+            // 授信额度增加
+            if (balance.longValue() < 0) {
+                anCreditDetail.setDirection("0");// 方向：0-收;1-支;
+            }
+            // 授信额度减少
+            if (balance.longValue() > 0) {
+                anCreditDetail.setDirection("1");// 方向：0-收;1-支;
+            }
+            // 变动额度
+            anCreditDetail.setBalance(balance.abs());
+            // 数据存盘-授信额度调整明细
+            scfCreditDetailService.insert(anCreditDetail);
+        }
+
+        return anModiCredit;
+    }
+
+    private void checkOperator(String anOperOrg, String anMessage) {
+        if (BetterStringUtils.equals(UserUtils.getOperatorInfo().getOperOrg(), anOperOrg) == false) {
+            logger.warn(anMessage);
+            throw new BytterTradeException(40001, anMessage);
+        }
+    }
+
+    private void checkStatus(String anBusinStatus, String anTargetStatus, boolean anFlag, String anMessage) {
+        if (BetterStringUtils.equals(anBusinStatus, anTargetStatus) == anFlag) {
+            logger.warn(anMessage);
+            throw new BytterTradeException(40001, anMessage);
         }
     }
 
