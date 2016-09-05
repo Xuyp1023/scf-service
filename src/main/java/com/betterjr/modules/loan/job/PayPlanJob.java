a3444e127418855f13299a6b26523cf34403695c
Tangzw <tangzw@bytter.com>
2016-09-05 17:51:08 +0800

    定时任务

---
+++ src/main/java/com/betterjr/modules/loan/job/PayPlanJob.java
@@ -0,0 +1,42 @@
+package com.betterjr.modules.loan.job;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import com.betterjr.modules.loan.service.ScfPayPlanService;
+import com.dangdang.ddframe.job.api.JobExecutionMultipleShardingContext;
+import com.dangdang.ddframe.job.plugin.job.type.simple.AbstractSimpleElasticJob;
+
+@Service
+public class PayPlanJob extends AbstractSimpleElasticJob {
+    private final static Logger logger = LoggerFactory.getLogger(PayPlanJob.class);
+    
+    @Autowired
+    private ScfPayPlanService payPlanService;
+    
+    
+    @Value("${sms.begin.time}")
+    private String beginTime;
+    
+    @Value("${sms.end.time}")
+    private String endTime;
+    
+    @Override
+    public void process(JobExecutionMultipleShardingContext anParamJobExecutionMultipleShardingContext) {
+        logger.info("*********定时任务：开始计算利息!");
+        logger.info("*********定时任务：开始计算利息!");
+        logger.info("*********定时任务：开始计算利息!");
+        logger.info("*********定时任务：开始计算利息!");
+        payPlanService.saveAutoUpdateOverDue();
+        logger.info("*********定时任务：计算利息结束!");
+        logger.info("*********定时任务：计算利息结束!");
+        logger.info("*********定时任务：计算利息结束!");
+        logger.info("*********定时任务：计算利息结束!");
+        logger.info("*********定时任务：计算利息结束!");
+        logger.info("*********定时任务：计算利息结束!");
+    }
+
+}
