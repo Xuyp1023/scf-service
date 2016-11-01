4dc6adf012cc6f90446f313ca30f8ff64fc8f2ad
root <git4bytter@sina.cn>
2016-11-01 11:43:21 +0800

    delete test

--- src/main/java/com/betterjr/modules/test/dao/TestEntityObjectMapper.java
+++
@@ -1,9 +0,0 @@
-package com.betterjr.modules.test.dao;
-
-import com.betterjr.common.annotation.BetterjrMapper;
-import com.betterjr.mapper.common.Mapper;
-import com.betterjr.modules.test.entity.TestEntityObject;
-
-@BetterjrMapper
-public interface TestEntityObjectMapper extends Mapper<TestEntityObject> {
-}
