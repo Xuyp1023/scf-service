4d46b1fa9d572126953dde771631a3c577bb246f
Xuyp <xuyp@bytter.com>
2017-03-29 08:29:43 +0800

    运行环境

--- src/main/java/com/betterjr/modules/approval/service/supply/ScfSupplyApplicationService.java
+++ src/main/java/com/betterjr/modules/approval/service/supply/ScfSupplyApplicationService.java
@@ -13,8 +13,10 @@ public class ScfSupplyApplicationService extends ScfBaseApprovalService{
 	
 	public ScfRequest savApplication(ScfRequest anRequest){
 		anRequest.setCustType(REQUEST_CUST_TYPE_SUPPLY);
-		anRequest = requestService.addRequest(anRequest);
+		anRequest = requestService.addRequest(anRequest);//新增融资t_scf_request并且 填充各个名称信息
+		//设置当前融资的状态
         this.updateAndSendRequestStatus(anRequest.getRequestNo(), RequestTradeStatus.OFFER_SCHEME.getCode(), RequestLastStatus.REQUEST.getCode());
+        //关联数据来源(订单，应收账单等信息，并且设置订单状态，公司授信等信息)
         this.forzenSource(anRequest);
         this.pushOrderInfo(requestService.findRequestByRequestNo(anRequest.getRequestNo()));
 		return anRequest;
