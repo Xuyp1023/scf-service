5c99bbb91e5e2eb1792848d6b921305eb5c58495
liuwl <liuwl@bytter.com>
2017-05-06 18:05:26 +0800

    =1

--- src/main/java/com/betterjr/modules/commission/dao/CommissionPayResultRecordMapper.java
+++ src/main/java/com/betterjr/modules/commission/dao/CommissionPayResultRecordMapper.java
@@ -18,9 +18,9 @@ import com.betterjr.modules.commission.entity.CommissionPayResultRecord;
 public interface CommissionPayResultRecordMapper extends Mapper<CommissionPayResultRecord> {
 
     @Insert("INSERT INTO t_cps_pay_result_record(ID, C_REFNO, L_PAY_RESULT_ID, L_RECORD_ID, C_RECORD_REFNO, "
-            + "D_IMPORT_DATE, T_IMPORT_TIME, D_PAY_DATE, C_PAY_TARGET_BANK, C_PAY_TARGET_BANK_NAME, C_PAY_TARGET_BANK_ACCO, "
+            + "D_IMPORT_DATE, T_IMPORT_TIME, D_PAY_DATE, F_PAY_BALANCE, C_PAY_TARGET_BANK, C_PAY_TARGET_BANK_NAME, C_PAY_TARGET_BANK_ACCO, "
             + "C_PAY_TARGET_BANK_ACCO_NAME, L_CUSTNO, C_CUSTNAME, C_OPERORG, L_REG_OPERID, C_REG_OPERNAME, D_REG_DATE, T_REG_TIME, C_PAY_RESULT, C_BUSIN_STATUS)  "
-            + "SELECT ID,C_REFNO,#{payResultId}, ID,C_REFNO, D_IMPORT_DATE, T_IMPORT_TIME, #{payDate}, C_BANK, C_BANK_NAME, C_BANK_ACCOUNT, C_BANK_ACCOUNT_NAME, "
+            + "SELECT ID,C_REFNO,#{payResultId}, ID,C_REFNO, D_IMPORT_DATE, T_IMPORT_TIME, #{payDate}, F_BALANCE, C_BANK, C_BANK_NAME, C_BANK_ACCOUNT, C_BANK_ACCOUNT_NAME, "
             + "#{custNo}, #{custName}, #{operOrg}, #{regOperId}, #{regOperName}, #{regDate}, #{regTime}, '0', '0' FROM t_cps_record tcr "
             + "WHERE tcr.L_CUSTNO = #{custNo} AND tcr.C_OPERORG = #{operOrg} AND tcr.D_IMPORT_DATE = #{importDate}")
     public int createPayResultRecord(Map<String, Object> param);
