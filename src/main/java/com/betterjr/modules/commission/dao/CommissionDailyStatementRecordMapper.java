ca3c4f69f651d2275df0f74f919b8fba6974e500
Hubl <hubl@bytter.com>
2017-05-09 18:10:33 +0800

    task-202-6

--- src/main/java/com/betterjr/modules/commission/dao/CommissionDailyStatementRecordMapper.java
+++ src/main/java/com/betterjr/modules/commission/dao/CommissionDailyStatementRecordMapper.java
@@ -14,11 +14,11 @@ public interface CommissionDailyStatementRecordMapper extends Mapper<CommissionD
             + "D_IMPORT_DATE,T_IMPORT_TIME,C_PAYNO,D_PAY_DATE,T_PAY_TIME,F_PAY_BALANCE,C_PAY_TARGET_BANK,C_PAY_TARGET_BANKNAME,C_PAY_TARGET_BANK_FULLNAME,"
             + "C_PAY_TARGET_BANK_ACCO,C_PAY_TARGET_BANK_ACCO_NAME,C_PAY_BANK,C_PAY_BANKNAME,C_PAY_BANK_FULLNAME,C_PAY_BANK_ACCO,C_PAY_BANK_ACCO_NAME,C_PAY_RESULT,"
             + "C_PAY_COMMENT,C_BUSIN_STATUS,C_LAST_STATUS,L_CUSTNO,C_CUSTNAME,C_OPERORG,L_REG_OPERID,C_REG_OPERNAME,D_REG_DATE,T_REG_TIME)"
-            + "SELECT ID,#{dailyStatement.id},#{dailyStatement.refNo},ID,C_REFNO,L_RECORD_ID,C_RECORD_REFNO,D_IMPORT_DATE,T_IMPORT_TIME,C_PAYNO,D_PAY_DATE,T_PAY_TIME"
-            + ",F_PAY_BALANCE,C_PAY_TARGET_BANK,C_PAY_TARGET_BANKNAME,C_PAY_TARGET_BANK_FULLNAME,C_PAY_TARGET_BANK_ACCO,C_PAY_TARGET_BANK_ACCO_NAME,C_PAY_BANK,"
-            + "C_PAY_BANK_NAME,C_PAY_BANK_FULLNAME,C_PAY_BANK_ACCO,C_PAY_BANK_ACCO_NAME,C_PAY_RESULT,C_PAY_COMMENT,#{dailyStatement.businStatus},"
-            + "#{dailyStatement.businStatus},#{dailyStatement.ownCustNo},#{dailyStatement.ownCustName},,#{dailyStatement.operOrg},#{dailyStatement.regOperId},"
-            + "#{dailyStatement.regOperName},#{dailyStatement.regDate},#{dailyStatement.regTime} FROM t_cps_pay_result_record"
-            + " WHERE d_pay_date=#{dailyStatement.payDate} and l_custno=#{dailyStatement.ownCustNo}")
+            + " SELECT ID,#{id},#{refNo},L_PAY_RESULT_ID,C_REFNO,L_RECORD_ID,C_RECORD_REFNO,D_IMPORT_DATE,T_IMPORT_TIME,C_PAYNO,D_PAY_DATE,T_PAY_TIME"
+            + ",F_PAY_BALANCE,C_PAY_TARGET_BANK,C_PAY_TARGET_BANK_NAME,C_PAY_TARGET_BANK_FULLNAME,C_PAY_TARGET_BANK_ACCO,C_PAY_TARGET_BANK_ACCO_NAME,C_PAY_BANK,"
+            + "C_PAY_BANK_NAME,C_PAY_BANK_FULLNAME,C_PAY_BANK_ACCO,C_PAY_BANK_ACCO_NAME,C_PAY_RESULT,C_PAY_COMMENT,#{businStatus},"
+            + "#{businStatus},#{ownCustNo},#{ownCustName},#{operOrg},#{regOperId},"
+            + "#{regOperName},#{regDate},#{regTime} FROM t_cps_pay_result_record"
+            + " WHERE d_pay_date=#{payDate} and l_custno=#{ownCustNo}")
     public int addDailyStatementRecord(CommissionDailyStatement dailyStatement);
 }
