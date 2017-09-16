e9de8584f4d57216f45c0d7a071c11bd0c5e6483
zhoucy <zhoucy@bytter.com>
2017-09-16 15:31:14 +0800

    电子合同查询，去掉电子合同类型

--- src/main/java/com/betterjr/modules/agreement/service/ScfElecAgreementService.java
+++ src/main/java/com/betterjr/modules/agreement/service/ScfElecAgreementService.java
@@ -118,19 +118,19 @@ public class ScfElecAgreementService extends BaseService<ScfElecAgreementMapper,
             termMap.put("signStatus", signStatus);
         }
         if (UserUtils.coreUser()) {
-            termMap.put("agreeType", Arrays.asList("1", "2"));
+            // termMap.put("agreeType", Arrays.asList("1", "2"));
             termMap.put("buyerNo", anParam.get("custNo"));
         }
         else if (UserUtils.supplierUser()) {
-            termMap.put("agreeType", Arrays.asList("0"));
+            // termMap.put("agreeType", Arrays.asList("0"));
             termMap.put("supplierNo", anParam.get("custNo"));
         }
         else if (UserUtils.sellerUser()) {
-            termMap.put("agreeType", Arrays.asList("2"));
+            // termMap.put("agreeType", Arrays.asList("2"));
             termMap.put("supplierNo", anParam.get("custNo"));
         }
         else if (UserUtils.factorUser()) {
-            termMap.put("agreeType", Arrays.asList("0", "1", "2"));
+            // termMap.put("agreeType", Arrays.asList("0", "1", "2"));
             termMap.put("factorNo", anParam.get("custNo"));
         }
         final List<Long> custNoList = UserUtils.findCustNoList();
