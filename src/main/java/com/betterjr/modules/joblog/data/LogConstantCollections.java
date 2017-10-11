b24eb3e2403c21caedb832beee1ed58bd736063e
Xuyp <xuyp@bytter.com>
2017-10-11 16:24:18 +0800

    融资申请过期任务

--- src/main/java/com/betterjr/modules/joblog/data/LogConstantCollections.java
+++ src/main/java/com/betterjr/modules/joblog/data/LogConstantCollections.java
@@ -9,5 +9,7 @@ public class LogConstantCollections {
     public final static String LOG_BUSIN_STATUS_SUCCESS = "1"; // 0 设置失败 1 设置成功
 
     public final static int LOG_RECORD_SIZE = 100; // 日志集合最大长度
+    
+    public final static String LOG_DATA_TYPE_RECEIVABLE_REQUEST = "6"; // 数据类型：1订单2票据3应收账款4发票5贸易合同  6应收账款申请
 
 }
